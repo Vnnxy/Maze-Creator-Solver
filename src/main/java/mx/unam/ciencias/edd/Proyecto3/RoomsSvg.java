adaf3be2ceb0454ac8090732f536745234c6a1d0
Akira <akira@Akiras-MacBook-Air.local>
2023-05-08 19:55:08 -0600

    svg idea

---
+++ src/main/java/mx/unam/ciencias/edd/Proyecto3/RoomsSvg.java
@@ -0,0 +1,12 @@
+package mx.unam.ciencias.edd.Proyecto3;
+
+public class RoomsSvg {
+
+    private String eastRoom = "";
+    private String westRoom = "<svg width='100' height='100'> \n <path d='M 15 10 L 85 10 L 85 90 L 15 90' stroke='black' fill='none' />\n </svg>";
+    private String northRoom = "";
+    private String southRoom = "";
+    private String entrance = "";
+    private String exit = "";
+
+}
