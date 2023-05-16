61217e8240d4e168465db15799e1a0a8f8f2e1d8
Akira <akira@Akiras-MacBook-Air.local>
2023-05-15 19:40:44 -0600

    first four bytes

--- src/main/java/mx/unam/ciencias/edd/Proyecto3/FileFormat.java
+++ src/main/java/mx/unam/ciencias/edd/Proyecto3/FileFormat.java
@@ -2,8 +2,11 @@ package mx.unam.ciencias.edd.Proyecto3;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.BufferedOutputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class FileFormat {
@@ -15,24 +18,34 @@ public class FileFormat {
     private String body = "";
 
     /* Array containing the first four bytes that our program requires. */
-    private Byte[] firstFour = { 0x4d, 0x41, 0x5a, 0x45 };
+    private byte[] firstFour = { 0x4d, 0x41, 0x5a, 0x45 };
 
     /* The method we'll use to write the first four bytes when creating a file */
     public String bytesMaze() {
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            out.write(firstFour);
+            String streamData = out.toString();
+            return streamData;
 
+        } catch (IOException ioe) {
+            // error
+        }
+        return null;
     }
 
     /*
-     * The method we'll use to check if a file contains the first four required
-     * bytes, thus making it valid.
+     * The method we will use to write the bytes that represent the dimensions we
+     * will be working with.
      */
-    public boolean hasMaze(byte[] file) {
-        String line = null;
-        try {
-            new ByteArrayInputStream(file)
-        } catch (IOException ioe) {
-            // I probably need a message here
-        }
+
+    public String getDimensions(int w, int h) {
+        return Integer.toHexString(w) + Integer.toHexString(h);
+    }
+
+    /* Private class we will use to ensure the rooms have all the requirements */
+    private class RoomFormat {
+
     }
 
 }
--- src/main/java/mx/unam/ciencias/edd/Proyecto3/Proyecto3.java
+++ src/main/java/mx/unam/ciencias/edd/Proyecto3/Proyecto3.java
@@ -2,6 +2,8 @@ package mx.unam.ciencias.edd.Proyecto3;
 
 public class Proyecto3 {
     public static void main(String[] args) {
-
+        FileFormat ff = new FileFormat();
+        ff.bytesMaze();
+        System.out.println(ff.getDimensions(8, 13));
     }
 }
